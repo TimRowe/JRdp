a22aea0a8cadd4f6a496fe2723af7df824af6990
303051991@qq.com <303051991@qq.com>
2019-11-14 16:41:04 +0800

    +)增加Mybatis生成SQL日志输出及Controller使用slf4日志输出

--- rdp-application/src/main/java/com/rdp/application/controller/TestController.java
+++ rdp-application/src/main/java/com/rdp/application/controller/TestController.java
@@ -1,10 +1,7 @@
 package com.rdp.application.controller;
-
-
-
-import com.rdp.system.mapper.*;
 import com.rdp.system.service.*;
-
+import jdk.internal.net.http.common.Log;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
  * @author 10011531
  */
 @RestController
+//@Slf4j需要使用lombok插件
+@Slf4j
 public class TestController {
 
     class JsonObj{
@@ -34,6 +33,7 @@ public class TestController {
 
     @GetMapping(value = "/test1")
     public String test1() {
+        log.debug("TestController/test1");
         return userService.selectByPrimaryKey("10030753").getUserName();
     }
 
